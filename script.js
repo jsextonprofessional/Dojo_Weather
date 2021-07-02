9aa30e7e8f3334469979a0b1fbc35a831a265923
jsprofessional <jshuman@protonmail.com>
2021-07-02 17:38:17 -0500

    functional, not finished, idk

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ for (var i = 0; i < temps.length; i++) {
 
 function changeUnit(element) {
     unit = element.value
-    convertToFahrenheit()
+    convertToFahrenheit(unit)
 }
 
-function convertToFahrenheit() {
-
-    for (var i = 0; i < temps.length; i++) {
-        temps[i].innerHTML = Math.round((temps[i].innerHTML * 1.8 + 32) * 10) / 10
+function convertToFahrenheit(unit) {
+    if (unit == 'F'){
+        for (var i = 0; i < temps.length; i++) {
+            temps[i].innerHTML = Math.round((temps[i].innerHTML * 1.8 + 32) * 10) / 10
+        }
+    } else {
+        for (var i = 0; i < temps.length; i++) {
+            temps[i].innerHTML = Math.round(((temps[i].innerHTML - 32) * .5556) * 10) / 10
+        }
     }
 }
